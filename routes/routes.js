a08f12ac2601009dfe6d23919fffee14826f4f81
Eren Tuna AÇIKBAŞ <erentuna26@icloud.com>
2021-02-25 23:19:40 +0300

    new routes added.

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.send('API is working properly !');
-});
+// Require controllers.
+var movie_controller = require("../controllers/movie_controller");
+
+// ! Routes
+
+// Get API home page.
+router.get("/", movie_controller.index);
+
+// * Requests by Name
+
+// Get Request for searching movies by Name.
+router.get("/movie/search/:name", movie_controller.movie_search_byName);
+
+// * GET REQUESTS by ID
+
+// Searching movies
+router.get("/movie/search/id/:id", movie_controller.movie_search_byID);
+
+// Getting movie backdrop
+router.get("/movie/image/backdrop/:size/:picturePath", movie_controller.movie_backdrop_image);
+
+// Getting movie cover
+router.get("/movie/image/cover/:size/:picturePath", movie_controller.movie_cover_image);
+
+// Recommendation
+router.get("/movie/recommendations/:id", movie_controller.movie_recommendations);
+
+// * //
+
+// Top Rated Movies
+router.get("/movie/top_rated/", movie_controller.movie_top_rated);
 
 module.exports = router;
